178667f8038a981e8aec691265a3c58898cb7209
Frostysnacks <rbernier93@gmail.com>
2023-06-08 20:34:23 -0400

    removed comments from review lol and added some type defs

--- server/models/Review.js
+++ server/models/Review.js
@@ -18,25 +18,6 @@ const reviewSchema = new Schema({
         default: Date.now,
         get: (timestamp) => dateFormat(timestamp),
     },
-    comments: [
-        {
-          commentText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxlength: 280,
-          },
-          commentAuthor: {
-            type: String,
-            required: true,
-          },
-          createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (timestamp) => dateFormat(timestamp),
-          },
-        },
-      ],
 });
 
 const Review = model('Review', reviewSchema);
