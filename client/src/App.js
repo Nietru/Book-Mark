9cf434f96c62b16f41a7edbaa10c0ef4640fea5c
Tifannie Truman <nietru143@gmail.com>
2023-06-11 16:22:31 -0400

    added apollo client and graphql to client deps

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
-import React, { useState, useEffect } from "react";
-// we have to return valid JSX from our components
-const App = () => {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+// TODO: import pages once created!!!
+
+// import Home from "./pages/Home";
+// import Matchup from "./pages/Matchup";
+// import Vote from "./pages/Vote";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
+function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+          <Routes>
+            {/* TODO: route paths!!! */}
+
+            {/* <Route path="/" element={<Home />} />
+            <Route path="/matchup" element={<Matchup />} />
+            <Route path="/matchup/:id" element={<Vote />} />
+            */}
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
-};
-// exporting a reference to the function.
+}
+
 export default App;
