bdb4e5b7f82f2a9928d9012eecc90dc51e00886e
Tifannie Truman <nietru143@gmail.com>
2023-06-11 15:49:18 -0400

    added ratings code to review schema for end of project

--- server/models/Review.js
+++ server/models/Review.js
@@ -1,25 +1,32 @@
-const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat.js');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat.js");
 
 const reviewSchema = new Schema({
-    reviewText: {
-        type: String,
-        required: 'You need to leave a review!',
-        minlength: 1,
-        maxlength: 500,
-    },
-    reviewAuthor: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (timestamp) => dateFormat(timestamp),
-    },
+  reviewText: {
+    type: String,
+    required: "You need to leave a review!",
+    minlength: 1,
+    maxlength: 500,
+  },
+  reviewAuthor: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  // TODO: may implement ratings in the future:
+  // rating: {
+  //     type: Number,
+  //     min: 0,
+  //     max: 10,
+  //     required: true,
+  //   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
 });
 
-const Review = model('Review', reviewSchema);
+const Review = model("Review", reviewSchema);
 
 module.exports = Review;
