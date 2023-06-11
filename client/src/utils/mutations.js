c6589bd931f0b060672402ce345f51331f0f44fd
Tifannie Truman <nietru143@gmail.com>
2023-06-11 16:49:54 -0400

    added login and signup components and edited mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,30 +1,25 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
-//login using email or username
 export const LOGIN = gql`
- mutation login($username: String, $password: String!, $email: String){
-     login(username: $username, password: $password, email: $email){ 
-        token
-        user{
-        _id
-        }
-     }
-}`;
-
-export const ADD_USER = gql`
-  mutation addUser(
-    $userName: String!
-    $email: String!
-    $password: String!
-  ) {
-    addUser(
-      userName: $userName
-      email: $email
-      password: $password
-    ) {
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
       token
       user {
         _id
+        username
       }
     }
-}`;
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($userName: String!, $email: String!, $password: String!) {
+    addUser(userName: $userName, email: $email, password: $password) {
+      token
+      user {
+        _id
+        userName
+      }
+    }
+  }
+`;
