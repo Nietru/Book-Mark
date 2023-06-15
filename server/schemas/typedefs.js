80dbf4ef66692518b919a5dbe2f6728746ff21a1
Tifannie Truman <nietru143@gmail.com>
2023-06-15 17:09:41 -0400

    finished client side queries and mutations

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,45 +1,47 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User{
-      _id: ID
-      username: String
-      email: String
-      reviews: [Review]
-    }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    reviews: [Review]
+  }
 
-    type Book{
-      _id: ID
-      bookId: String
-      title: String
-      image: String
-      reviews: [Review]
-    }
-    
-    type Review{
-      _id: ID
-      reviewText: String
-      reviewAuthor: String
-      createdAt: String
-    }
+  type Book {
+    _id: ID
+    bookId: String
+    title: String
+    image: String
+    reviews: [Review]
+  }
 
-    type Auth {
-      token: ID
-      user: User
-    }
+  type Review {
+    _id: ID
+    reviewText: String
+    reviewAuthor: String
+    createdAt: String
+    book: Book
+  }
 
-    type Query {
-      me: User
-      user(userId: ID!): User
-      review(reviewId: ID!): Review
-    }
-    
-    type Mutation {
-      addUser(username: String!, email: String!, password: String!): Auth
-      createReview(reviewText: String!): Review
-      login(username: String!, password: String!): Auth
-      # removeReview(reviewId: ID!): Review
-      # updateReview(reviewId: ID!, reviewText: String!): Review
-    }`;
+  type Auth {
+    token: ID
+    user: User
+  }
+
+  type Query {
+    me: User
+    user(userId: ID!): User
+    review(reviewId: ID!): Review
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    createReview(reviewText: String!): Review
+    login(username: String!, password: String!): Auth
+    # removeReview(reviewId: ID!): Review
+    # updateReview(reviewId: ID!, reviewText: String!): Review
+  }
+`;
 
 module.exports = typeDefs;
