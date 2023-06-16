06f214e6ce0b0ad95c761f9d3b7d93e8970b9c04
Tifannie Truman <nietru143@gmail.com>
2023-06-16 16:45:03 -0400

    created a header component and added to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Header from "./components/Header";
 import AppNavbar from "./components/Navbar";
 import SearchBooks from "./pages/SearchBooks";
 import Login from "./pages/Login";
@@ -17,11 +19,25 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem("id_token");
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
         <>
+          <Header></Header>
           <AppNavbar></AppNavbar>
           <div className="flex-column justify-center align-center min-100-vh bg-primary">
             <Routes>
