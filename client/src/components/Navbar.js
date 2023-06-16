e7182e53bb0d9d38c2b59c6937babc5b40865ba0
Tifannie Truman <nietru143@gmail.com>
2023-06-15 23:49:55 -0400

    need to setup reviewList and setup in navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -23,8 +23,8 @@ const AppNavbar = () => {
             {/* if user is logged in show saved books and logout */}
             {Auth.loggedIn() ? (
               <>
-                <Nav.Link as={Link} to="/saved">
-                  See Your Books
+                <Nav.Link as={Link} to="/reviewList">
+                  See Your Reviews!
                 </Nav.Link>
                 <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
               </>
